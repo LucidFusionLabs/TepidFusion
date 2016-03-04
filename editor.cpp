d772d900b5beb6523c26dceae322ef4e9608beee
p <koldfuzor@gmail.com>
2016-03-04 12:49:47 -0800

    all major ifdefs moved to static libraries

--- editor.cpp
+++ editor.cpp
@@ -16,26 +16,24 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "lfapp/lfapp.h"
-#include "lfapp/dom.h"
-#include "lfapp/css.h"
-#include "lfapp/flow.h"
-#include "lfapp/gui.h"
+#include "core/app/app.h"
+#include "core/web/dom.h"
+#include "core/web/css.h"
+#include "core/app/flow.h"
+#include "core/app/gui.h"
 
 namespace LFL {
 DEFINE_string(project, "",  "CMake compile_commands.json");
 DEFINE_int   (width,   840, "Window width");
 DEFINE_int   (height,  760, "Window height");
 
-BindMap *binds;
-bool console_animating = 0;
-
 struct EditorGUI : public GUI {
   TabbedDialog<EditorDialog> tabs;
   unique_ptr<IDE::Project> project;
   vector<MenuItem> context_menu{ MenuItem{ "", "Go To Definition", "gotodef" } };
+  bool console_animating = 0;
 
-  EditorGUI() : GUI(screen), tabs(this) { Activate(); }
+  EditorGUI() : tabs(this) { Activate(); }
   Editor *Top() { return tabs.top ? &tabs.top->editor : 0; }
 
   void GotoDefinition() {
@@ -55,11 +53,11 @@ struct EditorGUI : public GUI {
     static string prefix = "file://";
     string fn = PrefixMatch(fin, prefix) ? fin.substr(prefix.size()) : fin;
     INFO("Editor Open ", fn);
-    Font *font = Fonts::Get(FLAGS_default_font, "", FLAGS_default_font_size, Color::black, Color::white, FLAGS_default_font_flag);
-    EditorDialog *editor = new EditorDialog(screen, font, new LocalFile(fn, "r"), 1, 1); 
+    Font *font = app->fonts->Get(FLAGS_default_font, "", FLAGS_default_font_size, Color::black, Color::white, FLAGS_default_font_flag);
+    EditorDialog *editor = new EditorDialog(screen->gd, font, new LocalFile(fn, "r"), 1, 1); 
     editor->editor.project = project.get();
     editor->editor.line.SetAttrSource(&editor->editor);
-    editor->editor.SetColors(Singleton<TextGUI::SolarizedLightColors>::Get());
+    editor->editor.SetColors(Singleton<TextBox::SolarizedLightColors>::Get());
     editor->deleted_cb = [=](){ tabs.DelTab(editor); child_box.Clear(); delete editor; };
     if (auto c = editor->editor.bg_color) screen->gd->ClearColor((editor->color = *c));
     tabs.AddTab(editor);
@@ -71,7 +69,7 @@ struct EditorGUI : public GUI {
     Reset();
     box = tabs.box = screen->Box();
     tabs.Layout();
-    if (!child_box.Size()) child_box.PushBack(Box(), Drawable::Attr(), (Texture*)nullptr);
+    if (!child_box.Size()) child_box.PushNop();
   }
 
   int Frame(LFL::Window *W, unsigned clicks, int flag) {
@@ -81,48 +79,58 @@ struct EditorGUI : public GUI {
     screen->DrawDialogs();
     return 0;
   }
-} *editor_gui;
 
-void UpdateAnimating() { app->scheduler.SetAnimating(console_animating); }
-void OnConsoleAnimating() { console_animating = screen->lfapp_console->animating; UpdateAnimating(); }
+  void UpdateAnimating() { app->scheduler.SetAnimating(console_animating); }
+  void OnConsoleAnimating() { console_animating = screen->console->animating; UpdateAnimating(); }
+};
+
+void MyWindowInit(Window *W) {
+  screen->width = FLAGS_width;
+  screen->height = FLAGS_height;
+  screen->caption = app->name;
+}
+
+void MyWindowStart(Window *W) {
+  EditorGUI *editor_gui = W->AddGUI(make_unique<EditorGUI>());
+  if (FLAGS_console) W->InitConsole(bind(&EditorGUI::OnConsoleAnimating, editor_gui));
+  W->frame_cb = bind(&EditorGUI::Frame, editor_gui, _1, _2, _3);
+  W->default_textbox = [=](){ return editor_gui->Top(); };
+
+  BindMap *binds = W->AddInputController(make_unique<BindMap>());
+  binds->Add('6', Key::Modifier::Cmd, Bind::CB(bind(&Shell::console, W->shell.get(), vector<string>())));
+  binds->Add('o', Key::Modifier::Cmd, Bind::CB([=](){ W->shell->console(vector<string>(1, "choose")); }));
+  binds->Add('s', Key::Modifier::Cmd, Bind::CB([=](){ W->shell->console(vector<string>(1, "save")); }));
+  binds->Add('w', Key::Modifier::Cmd, Bind::CB([=](){ W->shell->console(vector<string>(1, "wrap")); }));
+  binds->Add(Mouse::Button::_1, Key::Modifier::Ctrl, Bind::CB([=](){ app->LaunchNativeContextMenu(editor_gui->context_menu); }));
+
+  W->shell = make_unique<Shell>(nullptr, nullptr, nullptr);
+  W->shell->Add("choose",  [=](const vector<string>&) { app->LaunchNativeFileChooser(1,0,0,"open"); });
+  W->shell->Add("save",    [=](const vector<string>&) { editor_gui->Top()->Save();             app->scheduler.Wakeup(0); });
+  W->shell->Add("wrap",    [=](const vector<string>&) { editor_gui->Top()->ToggleShouldWrap(); app->scheduler.Wakeup(0); });
+  W->shell->Add("gotodef", [=](const vector<string>&) { editor_gui->GotoDefinition();          app->scheduler.Wakeup(0); });
+  W->shell->Add("open",    [=](const vector<string>&a){ editor_gui->Open(a.size() ? a[0]: ""); app->scheduler.Wakeup(0); });
+}
 
 }; // namespace LFL
 using namespace LFL;
 
-extern "C" void LFAppCreateCB() {
+extern "C" void MyAppInit() {
+  FLAGS_lfapp_video = FLAGS_lfapp_input = true;
   app->name = "LEdit";
   app->logfilename = StrCat(LFAppDownloadDir(), "ledit.txt");
-  binds = new BindMap();
-  editor_gui = new EditorGUI();
-  screen->width = FLAGS_width;
-  screen->height = FLAGS_height;
-  screen->binds = binds;
-  screen->caption = app->name;
-  screen->frame_cb = bind(&EditorGUI::Frame, editor_gui, _1, _2, _3);
-  screen->default_textgui = [=](){ return editor_gui->Top(); };
-  FLAGS_lfapp_video = FLAGS_lfapp_input = true;
+  app->window_start_cb = MyWindowStart;
+  app->window_init_cb = MyWindowInit;
+  app->window_init_cb(screen);
 }
 
-extern "C" int main(int argc, const char *argv[]) {
-  if (app->Create(argc, argv, __FILE__, LFAppCreateCB)) { app->Free(); return -1; }
+extern "C" int MyAppMain(int argc, const char* const* argv) {
+  if (app->Create(argc, argv, __FILE__)) return -1;
   screen->width = FLAGS_width;
   screen->height = FLAGS_height;
 
-  if (app->Init()) { app->Free(); return -1; }
+  if (app->Init()) return -1;
   app->scheduler.AddWaitForeverKeyboard();
   app->scheduler.AddWaitForeverMouse();
-  app->shell.command.push_back(Shell::Command("choose",  [](const vector<string>&){ app->LaunchNativeFileChooser(1,0,0,"open"); }));
-  app->shell.command.push_back(Shell::Command("save",    [](const vector<string>&){ editor_gui->Top()->Save();             app->scheduler.Wakeup(0); }));
-  app->shell.command.push_back(Shell::Command("wrap",    [](const vector<string>&){ editor_gui->Top()->ToggleShouldWrap(); app->scheduler.Wakeup(0); }));
-  app->shell.command.push_back(Shell::Command("gotodef", [](const vector<string>&){ editor_gui->GotoDefinition();          app->scheduler.Wakeup(0); }));
-  app->shell.command.push_back(Shell::Command("open",    [](const vector<string>&a){ if (a.size()) editor_gui->Open(a[0]); app->scheduler.Wakeup(0); }));
-  if (screen->lfapp_console) screen->lfapp_console->animating_cb = OnConsoleAnimating;
-
-  binds->Add(Bind('6', Key::Modifier::Cmd, Bind::CB(bind(&Shell::console, app->shell, vector<string>()))));
-  binds->Add(Bind('o', Key::Modifier::Cmd, Bind::CB([=](){ app->shell.console(vector<string>(1, "choose")); })));
-  binds->Add(Bind('s', Key::Modifier::Cmd, Bind::CB([=](){ app->shell.console(vector<string>(1, "save")); })));
-  binds->Add(Bind('w', Key::Modifier::Cmd, Bind::CB([=](){ app->shell.console(vector<string>(1, "wrap")); })));
-  binds->Add(Bind(Mouse::Button::_1, Key::Modifier::Ctrl, Bind::CB([=](){ app->LaunchNativeContextMenu(editor_gui->context_menu); })));
 
   vector<MenuItem> file_menu{ MenuItem{"o", "Open", "choose"}, MenuItem{"s", "Save", "save" } };
   app->AddNativeMenu("File", file_menu);
@@ -135,6 +143,9 @@ extern "C" int main(int argc, const char *argv[]) {
 
   int optind = Singleton<FlagMap>::Get()->optind;
   if (optind >= argc) { fprintf(stderr, "Usage: %s [-flags] <file>\n", argv[0]); return -1; }
+
+  app->StartNewWindow(screen);
+  EditorGUI *editor_gui = screen->GetGUI<EditorGUI>(0);
 
   if (FLAGS_project.size()) {
     LocalFile ccf(FLAGS_project, "r");
